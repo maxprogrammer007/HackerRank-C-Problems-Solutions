ed27f895f2c2feb1faa7adf49b6a4d6264b04c28
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2023-12-21 05:05:32 +0530

    Create Playing With Characters.c

---
+++ Playing With Characters.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char ch,s[15],sen[100];
+    scanf("%c",&ch);
+    scanf("\n");
+    gets(s);
+    scanf("\n");
+    gets(sen);
+    printf("%c",ch);
+    printf("\n%s",s);
+    printf("\n%s",sen);
+       
+    return 0;
+}
